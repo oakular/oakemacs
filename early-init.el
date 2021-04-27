07b2d340552589df7ce24d38b89abd527a09aad3
Callum Warrilow <callum_warrilow@outlook.com>
2021-04-27 14:44:28 +0100

    Update with latest changes

---
+++ early-init.el
@@ -0,0 +1,29 @@
+;; Remove UI elements before they flash onscreen
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+
+;; --- optimisations ---
+;; Prevent frame resizing on font size change to reduce startup time
+(setq frame-inhibit-implied-resize t)
+
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      initial-major-mode 'fundamental-mode
+      initial-scratch-message nil)
+
+(defconst WINDOWS (memq system-type '(cygwin windows-nt ms-dos)))
+
+(when WINDOWS
+  (setq w32-get-true-file-attributes nil
+	w32-pipe-read-delay 0
+	w32-pipe-buffer-size (* 64 1024)))
+
+
+;; Remove superfluous UI elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq use-file-dialog nil)
+
