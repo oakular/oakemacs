f3f9c311478fd76ddf15368092893a7e83b5f988
Callum Warrilow <callum_warrilow@outlook.com>
2021-09-12 21:32:09 +0100

    Move shell funcs to own file

---
+++ lisp/oak-shell.el
@@ -0,0 +1,15 @@
+;;; oak-shell.el --- Functions providing interaction with shell commands -*- lexical-binding: t -*-
+
+(defvar oak-shell "/bin/bash" "The default shell to be used.")
+
+(defun oak/term () "Opens an ansi-term buffer using the shell set by oak-shell"
+       (interactive)
+       (ansi-term oak-shell))
+
+(defun oak/build-shell-cmd (cmd-elements)
+  "Build a command string from the elements passed as parameter."
+  (mapconcat 'identity cmd-elements " "))
+
+(oak/define-global-keymap '("C-x tt" . oak/term))
+
+(provide 'oak-shell)
--- lisp/oak-youtube-dl.el
+++ lisp/oak-youtube-dl.el
@@ -1,23 +1,41 @@
 ;;; oak-youtube-dl.el --- An interface to youtube-dl for media download -*- lexical-binding: t -*-
 
+(require 'oak-shell)
+
+(defconst oak-youtube-dl-executable "youtube-dl")
+
+(defun oak/read-url ()
+  (read-string "URL: "))
+
 (defun oak/download-from-url-at-point ()
   "Downloads the media at the URL at point."
   (interactive)
   (start-process
    "*download*"
    "*download*"
-   "youtube-dl"
+   oak-youtube-dl-executable
    "-o"
    "$HOME/Videos/yt/%(uploader)s/%(title)s.%(ext)s"
    (shr-url-at-point nil)))
+
+(defun oak/download-from-url ()
+  "Downloads the media at the given URL."
+  (interactive)
+  (start-process
+   "*download*"
+   "*download*"
+   oak-youtube-dl-executable
+   "-o"
+   "$HOME/Videos/yt/%(uploader)s/%(title)s.%(ext)s"
+   (oak/read-url)))
 
 (defun oak/download-album-from-url ()
   "Downloads the album at the URL."
   (interactive)
   (start-process "*download-album*"
                  "*download-album*"
-                 "youtube-dl"
-                 (read-string "URL: ")
+                 oak-youtube-dl-executable
+                 (oak/read-url)
                  "-x"
                  "--audio-format"
                  "mp3"
@@ -26,11 +44,12 @@
                  "$HOME/Music/%(channel)s/%(album)s/%(playlist_index)s_%(title)s.%(ext)s"))
 
 (defun oak/download-song-from-url ()
+  "Downloads the song at the given URL."
   (interactive)
   (start-process "*download-song*"
                  "*download-song*"
-                 "youtube-dl"
-                 (read-string "URL: ")
+                 oak-youtube-dl-executable
+                 (oak/read-url)
                  "-x"
                  "--audio-format"
                  "mp3"
@@ -38,5 +57,16 @@
                  "-o"
                  "$HOME/Music/%(channel)s/%(album)s/%(title)s.%(ext)s"
                  "--no-playlist"))
+
+(defun oak/stream-video-from-url ()
+  "Streams the video at the URL to MPV."
+  (interactive)
+  (async-shell-command (oak/build-shell-cmd (list oak-youtube-dl-executable
+                                                      "-o"
+                                                      "-"
+                                                      (oak/read-url)
+                                                      "|"
+                                                      "mpv"
+                                                      "-"))))
 
 (provide 'oak-youtube-dl)
