9160724c3f8334d7391d8f05db08b0b331025b70
Callum Warrilow <callum_warrilow@outlook.com>
2021-09-25 21:58:59 +0100

    Prefer encryption of PAT

--- lisp/sburl.el
+++ lisp/sburl.el
@@ -1,11 +1,21 @@
+(require 'epa)
+(require 'url)
+
 (defconst sburl-domain "https://api.starlingbank.com")
 (defconst sburl-domain-suffix "/api/v2")
 
-(defconst sburl-pat "eyJhbGciOiJQUzI1NiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAC1QSW4DMQz7SuFzFHhf5tZbP9AHyLJSGJl6grFTFCj69zqdHEmJFKkfUXsXi8BbPfeB-1rbR8Z2PdP2KU6i3_McZn8J2bODFKUB662BbIwGF1MMuaTsvZzL_H0Tiw3BqqStVydRcYhFeRWdivFBINF2b-NtWwvv77VMb-mTtFkGQOMc2BgcxGAc5ET2IqNnzjy9x3bldihwpnGaHEhHGqx2HrK0Adi7khOix6KmYjZ6JeLeD5UqxkvrCJgDgiUTIErlQWvlFClMRptHYdpu_DhyJF12xvLyBLDW_mTGjq0jjbq1g8i4YiM-wHzk4E-eCupf_5T4_QOqR2rgaQEAAA.maFzsEPPluhSamzRPh71oGOZFAkAIimH4wzV-Cx3eoC4w06Zr7XFrnDzpfocaXAtxynflXO37LVVgN7JgB4HvynuIiONjuj6rKxAYyiZv-xgHxzqYNngtQNic25KlpVsqu2ZAu7QHgHe6lkruKKRRVs9dpU4-qwteBy5A_vkWA53txFic9TJKBpViG1AsXHibhuBTU4uycyqNbiabUKoGqRZf9KOIfDtXbJ-cS5g4LPrR-VkXaWFv7CoKpG2NRm9eIePITVcknl9u3SNqv3uG6Rik5g7YY7QnvqUyetIT1zrDjnEISSfP_zvaUDINFQyzQvrrSRbRsDq-F0hvhmhtuNH3KgyCDf77qI5-cYpNs9TnvROJCfILPc0GpSqANjnuR3nKmOybNmhOLpt4YzHIQdd_z7595UqHE8Tuc9N-RE4jAbP770mzW7Hmu-NboaN9gZ5nbFMceanaGsLqdYHXe1wd5VldproxrFvwMQIIJ4hPTSlc-hH2zX3Q1XtNLxcVNmEf1O5Wi5pYlpzTgDznZNmKjRIWI_9ETWrQ-dC4Us94C3ff5aNxxwDNSHiIba8NHw5kM_7bxlo7uI4rPcFjWCN_YgCRqK0pfbkF4EjOXn-uZDSdP25Ly8BKYN_SxViE8x8Xn0UjsEYMGj3alx0S0taCUdCbViG7kIPoaf9_M4")
+(defun sburl/get-pat ()
+  "Return the personal access token stored in the configuration folder."
+  (when (file-exists-p (expand-file-name "~/.config/sburl/config.gpg"))
+    (epa-decrypt-file (expand-file-name "~/.config/sburl/pat.gpg")
+                      (expand-file-name "~/.config/sburl/pat")))
+  (with-temp-buffer
+    (insert-file-contents "~/.config/sburl/pat")
+    (buffer-string)))
 
 (defvar account-uid nil)
 
-(defvar sburl-auth-header (concat "Bearer " sburl-pat))
+(defvar sburl-auth-header (concat "Bearer " (sburl/get-pat)))
 
 (defconst sburl-headers `(("Authorization" . ,sburl-auth-header)
                         ("Content-Type" . "application/json")))
@@ -13,11 +23,6 @@
 (defun sburl/build-endpoint (endpoint-suffix)
   "Return the full endpoint URL for the API call for the ENDPOINT-SUFFIX."
   (concat sburl-domain sburl-domain-suffix endpoint-suffix))
-
-(defun sburl/get-path (&optional file-path)
-  "Get the configuration file from FILE-PATH, falling back to the default location."
-  (with-temp-buffer
-    (insert-file-contents (or file-path "~/.config/sburl/config"))))
 
 (defun sburl/decode-response (response-buffer)
       (with-current-buffer response-buffer
