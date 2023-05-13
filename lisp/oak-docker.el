356c9b8d90537adb7264a0c52ff75903722837c0
oakular <callumwarrilow@proton.me>
2023-05-13 16:42:46 +0100

    Generalise cmd handler

--- lisp/oak-docker.el
+++ lisp/oak-docker.el
@@ -2,22 +2,26 @@
 ;;; Commentary:
 
 ;;; Code:
-(defun oak-docker/containers/ls ()
+(defun oak-docker/container/ls ()
   "Return a list of containers."
   (split-string
    (shell-command-to-string
     "docker container ls -a --format \"{{.Names}}\"")))
 
-(defun oak-docker/containers/choices ()
-  "Return a list of containers, as choice options."
-  (let ((containers (oak-docker/containers/ls)))
-  (mapcar (lambda (x) (cons (make-symbol x) (seq-position containers x))) containers)))
+(defun oak-docker/container/cmd (cmd)
+  (let ((containers (oak-docker/container/ls)))
+    (let ((oak-docker-focused-container
+           (read-string "Container: " (car containers) 'containers)))
+      (start-process (concat "docker " cmd) "*docker*" "docker" "container" cmd oak-docker-focused-container))))
 
 (defun oak-docker/container/stop ()
   (interactive)
-  (let ((containers (oak-docker/containers/ls)))
-    (let ((oak-docker-focused-container
-           (read-string "Container: " (car containers) 'containers)))
-      (async-shell-command (concat "docker container stop " oak-docker-focused-container)))))
+  (oak-docker/container/cmd "stop")
+  (message "Container stopped successfully"))
+
+(defun oak-docker/container/start ()
+  (interactive)
+  (oak-docker/container/cmd "start"))
+
 (provide 'oak-docker)
 ;;; oak-docker.el ends here.
