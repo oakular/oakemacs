98e8dcbb66eca6018a6f66b977b557b00fe81b58
oakular <callumwarrilow@proton.me>
2023-05-14 15:06:28 +0100

    Improve docker function names

--- lisp/oak-docker.el
+++ lisp/oak-docker.el
@@ -2,26 +2,26 @@
 ;;; Commentary:
 
 ;;; Code:
-(defun oak-docker/container/ls ()
+(defun oak-docker/--container-ls ()
   "Return a list of containers."
   (split-string
    (shell-command-to-string
     "docker container ls -a --format \"{{.Names}}\"")))
 
-(defun oak-docker/container/cmd (cmd)
-  (let ((containers (oak-docker/container/ls)))
+(defun oak-docker/--container-cmd (cmd)
+  (let ((containers (oak-docker/--container-ls)))
     (let ((oak-docker-focused-container
            (read-string "Container: " (car containers) 'containers)))
       (start-process (concat "docker " cmd) "*docker*" "docker" "container" cmd oak-docker-focused-container))))
 
 (defun oak-docker/container/stop ()
   (interactive)
-  (oak-docker/container/cmd "stop")
+  (oak-docker/--container-cmd "stop")
   (message "Container stopped successfully"))
 
 (defun oak-docker/container/start ()
   (interactive)
-  (oak-docker/container/cmd "start"))
+  (oak-docker/--container-cmd "start"))
 
 (provide 'oak-docker)
 ;;; oak-docker.el ends here.
