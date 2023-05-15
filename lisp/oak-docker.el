466b0eb358f6ff9a1ecf48066cdc0fa938877d89
Callum Warrilow <vna2@eurofins.com>
2023-05-15 07:42:32 +0100

    Add docstrings

--- lisp/oak-docker.el
+++ lisp/oak-docker.el
@@ -9,18 +9,21 @@
     "docker container ls -a --format \"{{.Names}}\"")))
 
 (defun oak-docker/--container-cmd (cmd)
+  "Run the docker container command, CMD, on a container read from the minibuffer."
   (let ((containers (oak-docker/--container-ls)))
     (let ((oak-docker-focused-container
            (read-string "Container: " (car containers) 'containers)))
       (start-process (concat "docker " cmd) "*docker*" "docker" "container" cmd oak-docker-focused-container))))
 
 (defun oak-docker/container-stop ()
+  "Stop the docker container read from the minibuffer."
   (interactive)
   (set-process-sentinel
    (oak-docker/--container-cmd "stop")
    (lambda (x y) (message (concat "Stopping container " y)))))
 
 (defun oak-docker/container-start ()
+  "Start the docker container read from the minibuffer."
   (interactive)
   (set-process-sentinel
    (oak-docker/--container-cmd "start")
