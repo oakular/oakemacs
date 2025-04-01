12fa33130ca790892959903f4220e4870823b13f
oakular <callumwarrilow@proton.me>
2025-04-01 09:11:48 +0100

    Add sqm scripts

---
+++ lisp/sqm.el
@@ -0,0 +1,24 @@
+;;; sqm.el --- some square area calc utilities
+;;; Commentary:
+
+;;; Code:
+
+(defconst PADDING-DEFAULT 0)
+
+(defun sqm/calc
+    (length width &optional padding-percentage)
+  "Calculates the square area given by LENGTH and WIDTH.
+WASTAGE-PERCENTAGE may be provided to pad the area calculated."
+  (unless padding-percentage (setq padding-percentage 0))
+  (*(* length width) (+ 1 padding-percentage)))
+
+(defun sqm/calc-many
+    (dimensions &optional padding-percentage)
+  ""
+  (seq-reduce
+   (lambda (x y)
+     (+ (sqm/calc (alist-get 'length y) (alist-get 'width y) padding-percentage) x))
+   dimensions 0))
+
+(provide 'sqm)
+;;; sqm.el ends here
